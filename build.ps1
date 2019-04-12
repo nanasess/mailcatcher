aa3feefba7c6871fd9fe769403e3a43aea1b7a6f
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-04-12 10:26:05 +0900

    Add Tag

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop';
 Write-Host Starting build
 
-docker build --pull -t nanasess/mailcatcher -f Dockerfile .
+docker build --pull -t nanasess/mailcatcher:windowsservercore -f Dockerfile .
 docker images
