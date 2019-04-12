c9e50eb87da264f3d366574e0706dfaf193a4ea2
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-04-12 17:24:19 +0900

    Using for windowsservercore

--- deploy.ps1
+++ deploy.ps1
@@ -2,4 +2,4 @@ Write-Host Starting deploy
 docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
 
 docker push nanasess/mailcatcher:windowsservercore
-docker push nanasess/mailcatcher:nanoserver
+#docker push nanasess/mailcatcher:nanoserver
