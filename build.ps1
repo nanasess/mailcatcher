c4890214a60351b639b95ffa45fa58faddd326f9
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-04-12 14:40:29 +0900

    Fix COPY

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 Write-Host Starting build
 
-docker build --pull -t nanasess/mailcatcher:windowsnanoserver -f windowsservercore/Dockerfile .
-docker images
+#docker build --pull -t nanasess/mailcatcher:windowsnanoserver -f windowsservercore/Dockerfile .
+#docker images
 
 docker build --pull -t nanasess/mailcatcher:nanoserver -f nanoserver/Dockerfile .
 docker images
