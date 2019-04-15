b515368da9802394305eaa41eff1b0a604be509d
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-04-15 11:04:18 +0900

    Add suffix

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 Write-Host Starting deploy
 docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
 
-#docker push nanasess/mailcatcher:windowsservercore
+docker push nanasess/mailcatcher:windowsservercore
 #docker push nanasess/mailcatcher:nanoserver
