1d9262997740b4cfd6650adaeebb3ffb8912b2b5
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-04-15 09:17:24 +0900

    Check version

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 Write-Host Starting build
 
-docker build --pull -t nanasess/mailcatcher:windowsnanoserver -f windowsservercore/Dockerfile .
-docker images
-
-#docker build --pull -t nanasess/mailcatcher:nanoserver -f nanoserver/Dockerfile .
+#docker build --pull -t nanasess/mailcatcher:windowsnanoserver -f windowsservercore/Dockerfile .
 #docker images
+
+docker build --pull -t nanasess/mailcatcher:nanoserver -f nanoserver/Dockerfile .
+docker images
